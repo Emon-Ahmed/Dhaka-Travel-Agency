30dd6008982d77374d2a9e6c8638d4cd78b65226
Emon Ahmed <technicalboyemon@gmail.com>
2021-10-29 06:58:05 +0600

    Updated

---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDIsd5Lp9Uewh1a1O08fHrQbFpN5hcyRQU",
+    authDomain: "dhaka-travel-agency.firebaseapp.com",
+    projectId: "dhaka-travel-agency",
+    storageBucket: "dhaka-travel-agency.appspot.com",
+    messagingSenderId: "390917704063",
+    appId: "1:390917704063:web:9676cf15d8b1ec0504724a"
+  };
+
+  export default firebaseConfig;
